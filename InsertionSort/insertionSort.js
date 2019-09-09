f3d1f2e9a77ecfed818fecb752e3a283b481c818
Ronald Mutebi <ronald.mutebi@andela.com>
2019-09-09 22:09:22 +0300

    Insertion sort implementation

--- InsertionSort/insertionSort.js
+++ InsertionSort/insertionSort.js
@@ -1,6 +1,7 @@
 //An array with 1 item is considered sorted, so we start with the second item
 //we compare the selected item with the items on the LHS of the selected item.
 
+
 let insertionSort = (inputArr) => {
     let length = inputArr.length;
     for(let i =1; i<length; i++ ){
